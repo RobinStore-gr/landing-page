15ab7d7b667a807ff941ceaa9fa320b68146acef
Nick Bellos <49724459+nickbel7@users.noreply.github.com>
2023-07-16 21:45:24 +0300

    Revert "Initial implementation for landing page (mobile + desktop)"

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,40 +1,10 @@
 import React from 'react'
-import { isMobile } from 'react-device-detect';
 
 export default function Avatar({ name, img }) {
   return (
-    <div 
-      style={{
-        // width: '10em', 
-        height: '20em',
-        top: 0, 
-        display: 'flex',
-        position: 'relative', 
-        flexDirection: 'column', 
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        marginBottom: '2em',
-        fontSize: isMobile ? '10px' : '15px',
-      }}>
-        <img 
-          style={{
-            width: isMobile ? 120 : 200, 
-            height: isMobile ? 120 : 200, 
-            borderRadius: '50%', 
-            boxShadow: '0px 0px 10px 0px rgba(50,50,50,0.35)', 
-            background: 'white',
-            marginBottom: '2em',
-          }} src={img} alt={name} />
-        <div 
-          style={{
-            color: 'white', 
-            fontSize: isMobile ? 22 : 25, 
-            fontFamily: 'Coolvetica',
-            fontWeight: '400', 
-            wordWrap: 'break-word', 
-            textAlign:'center'
-          }}>{ name.split(' ')[0]} {isMobile ?  ' ' : <br></br>} {name.split(' ')[1]}
-        </div>
+    <div style={{width: 230, height: 'fit-content', top: 0, position: 'relative', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-end', gap: 29, display: 'inline-flex', margin: '2em'}}>
+        <img style={{width: 230, height: 230, borderRadius: 9999}} src={img} alt={name} />
+        <div style={{color: 'white', fontSize: 32, fontWeight: '400', wordWrap: 'break-word', textAlign:'center'}}>{ name }</div>
     </div>
   )
 }
